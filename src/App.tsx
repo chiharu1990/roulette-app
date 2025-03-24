1f8044add1cd455cda9084f5ce2f694c0b23b9cb
chiharu1990 <chiharufukuyama.1990@gmail.com>
2025-03-24 17:20:06 +0900

    ページを再読み込みしたときにログイン情報を保持する

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Roulette } from './components/Roulette';
 import { Login } from './components/Login';
+import { auth } from './firebase/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
 
 export const App = () => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setIsAuthenticated(!!user);
+            setLoading(false);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    if(loading) {
+        return <div className="loading">Loading...</div>
+    }
 
     return(
         <div>
